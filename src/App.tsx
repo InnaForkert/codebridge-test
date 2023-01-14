3309e785ca8eedae221515fed711509d373b6129
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-14 02:37:09 +0200

    add: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Suspense, lazy } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router";
+
+const Home = lazy(() => import("./pages/Home"));
+const Article = lazy(() => import("./pages/Article"));
 
 function App() {
-  return <></>;
+  return (
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/atricles/:articleId" element={<Article />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
