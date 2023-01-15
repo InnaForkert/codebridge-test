d83214213c0e1ded7794246c67ac1ea44e888c07
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-15 15:15:27 +0200

    fix: hashrouter for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
 import { ThemeProvider } from "@mui/system";
@@ -13,10 +13,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="codebridge-test">
+    <HashRouter basename="codebridge-test">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
